93f1a00358d366732c8363ec0af0c48576dea193
Viidakko <Jaakko.Hietikko@student.oulu.fi>
2024-12-21 14:33:08 +0200

    puhelinluettelo backend step6 complete

--- index.js
+++ index.js
@@ -54,6 +54,20 @@ app.get('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
     const body = request.body
 
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    } else if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    } else if (persons.some(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
     const person = {
         id: generateId(),
         name: body.name,
