a514e63ff39ef7021e0fe7bea2bcb8ce916ad065
Viidakko <Jaakko.Hietikko@student.oulu.fi>
2025-01-06 21:04:46 +0200

    puhelinluettelo fullstack

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 app.use(cors())
+app.use(express.static('dist'))
 
 let persons = [
     {
